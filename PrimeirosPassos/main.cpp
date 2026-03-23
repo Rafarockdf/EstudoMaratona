a61c88f5793df26b35ffda5ef6d92d4bdb4af4e1
Rafael Luiz <127802178+Rafarockdf@users.noreply.github.com>
2026-03-23 23:13:35 +0000

    Primeiros passos estudo c++

---
+++ PrimeirosPassos/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Biblioteca padrão de entrada e saída
+
+int main() {
+    // std::cout é usado para exibir texto no console
+    std::cout << "Ola, Mundo!" << std::endl;
+    return 0; // Indica que o programa terminou com sucesso
+}
